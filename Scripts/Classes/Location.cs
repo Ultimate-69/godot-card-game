54ed43e1a1f9d679c0095899f437773ff3e95a36
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-06-05 02:08:46 +0300

    limit added cards to only 4, and used array for the cards.

--- Scripts/Classes/Location.cs
+++ Scripts/Classes/Location.cs
@@ -18,19 +18,59 @@ public partial class Location : Control
     public Label powerBottom;
     [Export]
     public Button locationButton;
+    [Export]
+    public GridContainer topGrid;
+    [Export]
+    public GridContainer bottomGrid;
 
     Card[] cardsTop = new Card[4];
     Card[] cardsBottom = new Card[4];
+
+    Tween tween;
 
     public override void _Ready()
     {
         base._Ready();
         locationButton.Pressed += () =>
         {
-            powerBottom.Text = (powerBottom.Text.ToInt() + CardEffects.selectedCard.cardResource.cardPower).ToString();
-            CardEffects.selectedCard.cardButton.Disabled = true;
-            CardEffects.ChangeSelectedCard(null);
+            if (AllocateCardToLocation(false))
+            {
+                powerBottom.Text = (powerBottom.Text.ToInt() + CardEffects.selectedCard.cardResource.cardPower).ToString();
+                CardEffects.selectedCard.cardButton.QueueFree();
+                CardEffects.selectedCard.Reparent(bottomGrid);
+                CardEffects.ChangeSelectedCard(null);
+            }
         };
+    }
+
+    // The bool manages if it allocates to the top array or bottom array.
+    // If it returns true, success. Otherwise, fail.
+    private bool AllocateCardToLocation(bool top)
+    {
+        if (top)
+        {
+            for (int i = 0; i < cardsTop.Length; i++)
+            {
+                if (cardsTop[i] == null)
+                {
+                    cardsTop[i] = CardEffects.selectedCard;
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < cardsBottom.Length; i++)
+            {
+                if (cardsBottom[i] == null)
+                {
+                    cardsBottom[i] = CardEffects.selectedCard;
+                    return true;
+                }
+            }
+        }
+        return false;
+
     }
 
 }
